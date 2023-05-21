3d6b76fb6258634323322274ada3988346b0bbc9
gulanovas <83650564+gulanovas@users.noreply.github.com>
2023-05-22 01:58:27 +0200

    Update forms.py

--- orders/forms.py
+++ orders/forms.py
@@ -6,12 +6,3 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'order_note']
-
-        def clean_email(self):
-            data = self.cleaned_data['email']
-        
-            # Check for the condition that should raise an error
-            if data != 'expected_value':
-                raise forms.ValidationError('Invalid value for email')
-
-            return data
